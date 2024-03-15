822d50c0c59fe1f264c7edd103839d4acd24b30c
Amitav Roy <reachme@amitavroy.com>
2024-03-15 06:50:15 +0530

    Missed these files
    Missed committing these files so adding them.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     <title>{{ config('app.name') }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
