dcdaf13c54a46ffb0636fe1e40c4dc0f9f2bc79c
Amitav Roy <reachme@amitavroy.com>
2024-03-16 15:24:15 +0530

    Refactored the web hook to use datasets
    Refactored the Gitlab web hook controller API tests to use data sets instead of separate tests

--- tests/Feature/GitlabWebhookApiTest.php
+++ tests/Feature/GitlabWebhookApiTest.php
@@ -6,45 +6,50 @@ use function Pest\Laravel\postJson;
 
 uses(RefreshDatabase::class);
 
-it('gives a 200 status when data is proper', function () {
+$dataSet = [
+    'webhook-gitlabcomment.json',
+    'webhook-gitlabissue.json',
+];
+
+it('gives a 200 status when data is proper', function (string $fileName) {
     // Arrange
     $sampleData = json_decode(
-        file_get_contents(__DIR__.'/../Fixtures/webhook-gitlabissue.json'), true
+        file_get_contents(__DIR__. "/../Fixtures/{$fileName}"), true
     );
 
     // Act & Assert
     postJson(route('gitlab-webhooks'), $sampleData, [
         'X-Gitlab-Token' => config('services.gitlab.secret_token'),
     ])->assertOk();
-});
+})->with($dataSet);
 
-it('gives a 401 error when the header is missing', function () {
+it('gives a 401 error when the header is missing', function (string $fileName) {
     // Arrange
     $sampleData = json_decode(
-        file_get_contents(__DIR__.'/../Fixtures/webhook-gitlabissue.json'), true
+        file_get_contents(__DIR__. "/../Fixtures/{$fileName}"), true
     );
 
     // Act & Assert
     postJson(route('gitlab-webhooks'), $sampleData)
         ->assertUnauthorized();
-});
+})->with($dataSet);
 
-it('gives a 401 when the token is wrong', function () {
+it('gives a 401 when the token is wrong', function (string $fileName) {
     // Arrange
     $sampleData = json_decode(
-        file_get_contents(__DIR__.'/../Fixtures/webhook-gitlabissue.json'), true
+        file_get_contents(__DIR__. "/../Fixtures/{$fileName}"), true
     );
 
     // Act & Assert
     postJson(route('gitlab-webhooks'), $sampleData, [
         'X-Gitlab-Token' => config('services.gitlab.secret_token').'-add-random',
     ])->assertUnauthorized();
-});
+})->with($dataSet);
 
-it('gives a 400 error when event type is not supported', function () {
+it('gives a 400 error when event type is not supported', function (string $fileName) {
     // Arrange
     $sampleData = json_decode(
-        file_get_contents(__DIR__.'/../Fixtures/webhook-gitlabissue.json'), true
+        file_get_contents(__DIR__. "/../Fixtures/{$fileName}"), true
     );
     $sampleData['event_type'] = 'random';
 
@@ -52,4 +57,4 @@ it('gives a 400 error when event type is not supported', function () {
     postJson(route('gitlab-webhooks'), $sampleData, [
         'X-Gitlab-Token' => config('services.gitlab.secret_token'),
     ])->assertBadRequest();
-});
+})->with($dataSet);
